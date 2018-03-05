dd2b2b51c90428e507feafbe903c04aac551c6c5
Brandon2018 <35273996+Brandon2018@users.noreply.github.com>
2018-03-05 13:32:04 -0800

    Add files via upload

---
+++ EventTest.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "Event.h"
+
+int main(){
+    Event eve;
+    cout << "***First time insertion (shows one event)" << endl;
+    eve.addEvent("Birthday", "Mark turns 5 years old", 3, 22, 2018, 3, 0, "P.M.");
+    eve.printMonth(3);
+    cout << "***Second time insertion (shows two events)" << endl;
+    eve.addEvent("Graduation", "Stacey's high school graduation", 3, 30, 2018, 7, 30, "P.M.");
+    eve.printMonth(3);
+    cout << "***Remove event (shows one event now)" << endl;
+    eve.removeEvent("Birthday", 3);
+    eve.printMonth(3);
+    cout << "***Third time insertion (shows one event for both months)" << endl;
+    eve.addEvent("Doc Appointment", "Follow up on tests", 1, 27, 2018, 9, 45, "A.M.");
+    cout << "**For January" << endl;
+    eve.printMonth(1);
+    cout << "**For March" << endl;
+    eve.printMonth(3);
+    cout << "***Deleting last event (shows no events for march)" << endl;
+    eve.removeEvent("Graduation", 3);
+    eve.printMonth(3);
+    
+    return 0;
+}
