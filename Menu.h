ecd7b98a5f5d49a9d036c0e8e95cac86d6586779
KeiXIII <35160856+KeiXIII@users.noreply.github.com>
2018-03-13 21:28:41 -0700

    Add files via upload

---
+++ Menu.h
@@ -0,0 +1,11 @@
+#ifndef  MENU_H
+#define MENU_H
+#include <iostream>
+#include <string>
+#include "Event.h"
+
+class Menu {
+public:
+	void start();
+};
+#endif // ! MENU_H
---
+++ reminder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Event.h"
+#include "Menu.h"
+
+using namespace std;
+int main()
+{
+	Menu menu;
+	menu.start();
+	system("pause");
+	return 0;
+}
